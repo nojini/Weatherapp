69fc99eb694c67acae243bb319bd37a35bc2067c
nojini <nkemjika.ojini@gmail.com>
2023-08-01 00:58:40 -0400

    Update Constants.kt

--- app/src/main/java/com/faskn/app/weatherapp/core/Constants.kt
+++ app/src/main/java/com/faskn/app/weatherapp/core/Constants.kt
@@ -8,14 +8,14 @@ object Constants {
 
     object NetworkService {
         const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
-        const val API_KEY_VALUE = "751d80f6c314139192ffcb62c107e654"
+        const val API_KEY_VALUE = "96527b8e56e593d8bae881cd0540bb3f"
         const val RATE_LIMITER_TYPE = "data"
         const val API_KEY_QUERY = "appid"
     }
 
     object AlgoliaKeys {
-        const val APPLICATION_ID = "plNW8IW0YOIN"
-        const val SEARCH_API_KEY = "029766644cb160efa51f2a32284310eb"
+        const val APPLICATION_ID = "LEK179A3SC"
+        const val SEARCH_API_KEY = "782c4c5049475aeae4904ea601932b5e"
     }
 
     object Coords {
