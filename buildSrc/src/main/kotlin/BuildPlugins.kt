ed66d43ecb98970ba684b2e4ad132c89aac140de
nojini <nkemjika.ojini@gmail.com>
2023-08-01 17:25:52 -0400

    Create BuildPlugins.kt

---
+++ buildSrc/src/main/kotlin/BuildPlugins.kt
@@ -0,0 +1,7 @@
+object BuildPlugins {
+    const val googleGradle = "com.google.gms:google-services:${Versions.googleGradleVersion}"
+    const val buildGradle = "com.android.tools.build:gradle:${Versions.buildGradleVersion}"
+    const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
+    const val navigationGradle = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigationGradleVersion}"
+    const val hiltGradle = "com.google.dagger:hilt-android-gradle-plugin:${Versions.hiltVersion}"
+}
