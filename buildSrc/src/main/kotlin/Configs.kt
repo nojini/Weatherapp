6db0ae5d490dcb1cbc484e4362b5371cafeee89f
nojini <nkemjika.ojini@gmail.com>
2023-08-01 21:53:59 -0400

    Upgrade Android SDK
    To Android 13

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -4,9 +4,9 @@ import java.util.*
 object Configs {
     const val applicationId = "com.faskn.app.weatherapp"
 
-    const val buildToolsVersion = "30.0.2"
-    const val compileSdkVersion = 30
-    const val targetSdkVersion = 30
+    const val buildToolsVersion = "33.0.1"
+    const val compileSdkVersion = 33
+    const val targetSdkVersion = 33
     const val minSdkVersion = 21
 
     const val versionCode = 3
